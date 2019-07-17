0f5edbcde850d4dccc8d2743837d35d243bdd089
Joshua Ji <joshua.ji@jiwhiz.com>
2019-07-17 16:31:54 -0600

    comment

--- src/Util.elm
+++ src/Util.elm
@@ -3,7 +3,7 @@ module Util exposing (toPairs)
 {-
    Utilities
 -}
--- pair up everything in the list. If there are an odd amount of objects return (a, Nothing) in the last element
+-- pair up everything in the list. If there are an odd amount of objects return (a, Nothing) in the last element. Mainly used just for the Projects page
 
 
 toPairs : List a -> List ( a, Maybe a )
