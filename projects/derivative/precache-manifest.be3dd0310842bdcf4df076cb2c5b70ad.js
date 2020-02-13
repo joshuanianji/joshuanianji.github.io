8b0146ed04007e88219ffc8f91359ff4c320f087
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-02-13 16:35:20 -0700

    added derivative project

---
+++ projects/derivative/precache-manifest.be3dd0310842bdcf4df076cb2c5b70ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "7c7a519777f5e89da2d4ab2b5d7129c5",
+    "url": "/icon.png"
+  },
+  {
+    "revision": "ad14eb67b40d20be030e94591d20326c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "d2062c43078d1ee6360da41f32bfe904",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e142a2fde72296f1a7da",
+    "url": "/static/css/main.6cf4b74a.chunk.css"
+  },
+  {
+    "revision": "e142a2fde72296f1a7da",
+    "url": "/static/js/main.e142a2fd.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  },
+  {
+    "revision": "27fc669e29b606108a86963b86410555",
+    "url": "/static/media/cmunrm.27fc669e.ttf"
+  }
+]);
