207da8318736ef120bc852f4deaf8b229ee49b64
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-06-24 21:44:38 -0600

    Lots of new changes to HIIT Timer! - moved to create-elm-app so it'll be a PWA - works on mobile (i think) - Some other changes

---
+++ projects/hiit-timer/precache-manifest.78e79d1125a9e0c76f4396fb9302a6dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "46c24498f58c20334404abdd245fdd70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c9259a376badc032c446",
+    "url": "/static/css/main.54652cf0.chunk.css"
+  },
+  {
+    "revision": "c9259a376badc032c446",
+    "url": "/static/js/main.c9259a37.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "c3e7e8df25ef01c83ef9",
+    "url": "/static/js/vendors~main.c3e7e8df.chunk.js"
+  },
+  {
+    "revision": "b5305f8bdef25a4099d2283cbb4ee35d",
+    "url": "/static/media/smh.b5305f8b.png"
+  },
+  {
+    "revision": "1768de81fa3eef9da9ce4aedc029bc69",
+    "url": "/static/media/tada.1768de81.mp3"
+  },
+  {
+    "revision": "9aefd12553b1666cbe345dd7c1d78fd2",
+    "url": "/static/media/tada.9aefd125.wav"
+  },
+  {
+    "revision": "63f8231867a8414be6b7425aa3f923b2",
+    "url": "/static/media/whistle.63f82318.mp3"
+  }
+]);
