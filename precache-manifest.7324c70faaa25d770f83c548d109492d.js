72455890bcbe86846b0931cd47a84b92686326d1
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-09-19 22:15:39 -0600

    Long overdue website redesign hopefully this works!

---
+++ precache-manifest.7324c70faaa25d770f83c548d109492d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d3babdf3c5c46b976ba91fd6ba61c8bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c5af90160c695d8e9056",
+    "url": "/static/css/main.0611510c.chunk.css"
+  },
+  {
+    "revision": "c5af90160c695d8e9056",
+    "url": "/static/js/main.c5af9016.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "f6de66643039766ba59d",
+    "url": "/static/js/vendors~main.f6de6664.chunk.js"
+  },
+  {
+    "revision": "3e8ce72108429f074e5c55373443d2a8",
+    "url": "/static/media/cli.3e8ce721.png"
+  },
+  {
+    "revision": "7c7a519777f5e89da2d4ab2b5d7129c5",
+    "url": "/static/media/derivatives.7c7a5197.png"
+  },
+  {
+    "revision": "1dea9580411f394fcc9b8285fea32a77",
+    "url": "/static/media/hiit-timer.1dea9580.png"
+  },
+  {
+    "revision": "a7b936a9d9a12f38c16cfd0fcf0c58da",
+    "url": "/static/media/ptable.a7b936a9.png"
+  },
+  {
+    "revision": "bce3e78dc43e55bb1eaed9dada99f302",
+    "url": "/static/media/ui-bootstrap.bce3e78d.png"
+  }
+]);
