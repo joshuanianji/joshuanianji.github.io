06d65a61898101be7150e3d06ee5ff0516f41ab2
joshuanianji <joshua.ji@jiwhiz.com>
2020-12-13 00:50:07 -0700

    Fixed build issue

---
+++ projects/hiit-timer/precache-manifest.13c74f6ae6c86bd9abbc58c1f3c50a71.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dcfa175b9835f299e0c1e4cb1b95b5",
+    "url": "/projects/hiit-timer/favicon-white-bg.png"
+  },
+  {
+    "revision": "1dea9580411f394fcc9b8285fea32a77",
+    "url": "/projects/hiit-timer/favicon.png"
+  },
+  {
+    "revision": "ef09c5ea2038cf9bc49f811577c5a9a8",
+    "url": "/projects/hiit-timer/index.html"
+  },
+  {
+    "revision": "8f3a3d29fd8401bea11569f8669c1a5f",
+    "url": "/projects/hiit-timer/manifest.json"
+  },
+  {
+    "revision": "d4e488b3ddd7d156d126",
+    "url": "/projects/hiit-timer/static/css/main.3a591fbe.chunk.css"
+  },
+  {
+    "revision": "d4e488b3ddd7d156d126",
+    "url": "/projects/hiit-timer/static/js/main.d4e488b3.chunk.js"
+  },
+  {
+    "revision": "9d4ae921b2a3f790ce5f",
+    "url": "/projects/hiit-timer/static/js/runtime~main.9d4ae921.js"
+  },
+  {
+    "revision": "1a289f9b9465d47b4056",
+    "url": "/projects/hiit-timer/static/js/vendors~main.1a289f9b.chunk.js"
+  },
+  {
+    "revision": "b5305f8bdef25a4099d2283cbb4ee35d",
+    "url": "/projects/hiit-timer/static/media/smh.d184a70a.png"
+  },
+  {
+    "revision": "1768de81fa3eef9da9ce4aedc029bc69",
+    "url": "/projects/hiit-timer/static/media/tada.9cfbe32b.mp3"
+  },
+  {
+    "revision": "9aefd12553b1666cbe345dd7c1d78fd2",
+    "url": "/projects/hiit-timer/static/media/tada.b49a92e1.wav"
+  },
+  {
+    "revision": "63f8231867a8414be6b7425aa3f923b2",
+    "url": "/projects/hiit-timer/static/media/whistle.4f149710.mp3"
+  },
+  {
+    "revision": "ad2f981820c1e26b9c95b61832af190a",
+    "url": "/projects/hiit-timer/version.txt"
+  }
+]);
