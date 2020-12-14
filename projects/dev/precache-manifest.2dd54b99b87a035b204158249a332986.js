881bfc73c00b11436868b5866d90bb29976ac6c7
joshuanianji <joshua.ji@jiwhiz.com>
2020-12-14 01:05:33 -0700

    Test WakeLock on HIIT-Timer

---
+++ projects/dev/precache-manifest.2dd54b99b87a035b204158249a332986.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dcfa175b9835f299e0c1e4cb1b95b5",
+    "url": "/projects/dev/favicon-white-bg.png"
+  },
+  {
+    "revision": "1dea9580411f394fcc9b8285fea32a77",
+    "url": "/projects/dev/favicon.png"
+  },
+  {
+    "revision": "6f5b4cad3e31031c1d0473906c860402",
+    "url": "/projects/dev/index.html"
+  },
+  {
+    "revision": "8f3a3d29fd8401bea11569f8669c1a5f",
+    "url": "/projects/dev/manifest.json"
+  },
+  {
+    "revision": "5590f78da6d923d9bbf0",
+    "url": "/projects/dev/static/css/main.3a591fbe.chunk.css"
+  },
+  {
+    "revision": "5590f78da6d923d9bbf0",
+    "url": "/projects/dev/static/js/main.5590f78d.chunk.js"
+  },
+  {
+    "revision": "7fb1436cb7767873d586",
+    "url": "/projects/dev/static/js/runtime~main.7fb1436c.js"
+  },
+  {
+    "revision": "e52175ea27f063fd0bf6",
+    "url": "/projects/dev/static/js/vendors~main.e52175ea.chunk.js"
+  },
+  {
+    "revision": "b5305f8bdef25a4099d2283cbb4ee35d",
+    "url": "/projects/dev/static/media/smh.d184a70a.png"
+  },
+  {
+    "revision": "1768de81fa3eef9da9ce4aedc029bc69",
+    "url": "/projects/dev/static/media/tada.9cfbe32b.mp3"
+  },
+  {
+    "revision": "9aefd12553b1666cbe345dd7c1d78fd2",
+    "url": "/projects/dev/static/media/tada.b49a92e1.wav"
+  },
+  {
+    "revision": "63f8231867a8414be6b7425aa3f923b2",
+    "url": "/projects/dev/static/media/whistle.4f149710.mp3"
+  }
+]);
