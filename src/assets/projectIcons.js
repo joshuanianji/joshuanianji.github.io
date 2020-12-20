85e5698987cad559bbc67e7153318c81ab336e74
joshuanianji <joshua.ji@jiwhiz.com>
2020-12-20 13:59:19 -0700

    🚚 Create assets/ folder

---
+++ src/assets/projectIcons.js
@@ -0,0 +1,67 @@
+// assets
+import cli from './img/proj_icons/cli.png';
+import derivativesSrc from './img/proj_icons/derivatives.png';
+import hiitTimerSrc from './img/proj_icons/hiit-timer.png';
+import ptableSrc from './img/proj_icons/ptable.png';
+import uiBootstrapSrc from './img/proj_icons/ui-bootstrap.png';
+import atomicTheory from './img/proj_icons/atomic-theory.png';
+import defaultIcon from './img/proj_icons/default_icon.png';
+
+
+let projIcons = [{
+    src: derivativesSrc,
+    id: 'derivative'
+}, {
+    src: hiitTimerSrc,
+    id: 'hiit-timer'
+}, {
+    src: ptableSrc,
+    id: 'ptable'
+}, {
+    src: uiBootstrapSrc,
+    id: 'ui-bootstrap'
+}, {
+    src: atomicTheory,
+    id: 'atomic-theory'
+}, {
+    src: cli,
+    id: 'cli'
+}];
+
+export {
+    defaultIcon,
+    projIcons
+}
+// here is the old school projects I did not include in my thing
+/*{
+        "name": "Country Fact Finder",
+        "imgLink": null,
+        "blurb": "An application that parses and displays information from 5 text files.",
+        "githubLink": "https://github.com/joshuanianji/Country-Fact-Finder",
+        "link": "https://joshuaji.com/projects/fact-finder",
+        "year": 2019,
+        "language": "Elm",
+        "concepts": [],
+        "pinned": false
+    },
+    {
+        "name": "WordRank",
+        "imgLink": null,
+        "blurb": "An application that ranks words based on usage from a string input or a text file.",
+        "githubLink": "https://github.com/joshuanianji/Wordrank",
+        "link": "https://joshuaji.com/projects/word-rank",
+        "year": 2019,
+        "language": "Elm",
+        "pinned": false
+    },
+    {
+        "name": "Cryptography",
+        "imgLink": null,
+        "blurb": "An app that deals with Caesar and Viginere cyphers, and can calculate hashes.",
+        "githubLink": "https://github.com/joshuanianji/Cryptography",
+        "link": "https://joshuaji.com/projects/cryptography",
+        "year": 2018,
+        "language": "Elm",
+        "pinned": false
+    }
+    */
