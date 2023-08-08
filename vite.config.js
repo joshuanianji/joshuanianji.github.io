63a57aa8408577c17a5f01549aa1c57e79a450fe
Joshua Ji <joshua.ji@jiwhiz.com>
2023-08-08 04:42:23 +0000

    Move new site outside of random inner folder
    i think this might help my git history later on

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import elmPlugin from 'vite-plugin-elm';
-
-export default defineConfig({
-    plugins: [elmPlugin()],
-    server: {
-        port: 3000,
-    },
-});
